42fe2711fa0bbc64a312bfa8dde07acd87e5e590
VictorWinberg <victor.m.winberg@gmail.com>
2018-05-01 22:12:36 +0200

    add reorder category

--- src/containers/categories/index.js
+++ src/containers/categories/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import CategoryList from './CategoryList';
+import Reorder from './Reorder';
 import New from '../common/New';
 import { addCategory } from '../../actions/categories';
 
@@ -8,6 +9,7 @@ const Categories = () => (
   <div>
     <New view="categories" onAdd={addCategory} />
     <CategoryList view="categories" />
+    <Reorder />
   </div>
 );
 
