c4ded288afc1cb91e8f8b4e13de4e0af84aa8698
VictorWinberg <victor.m.winberg@gmail.com>
2018-05-02 11:45:42 +0200

    add DnDList

--- src/containers/categories/index.js
+++ src/containers/categories/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import CategoryList from './CategoryList';
-import Reorder from './Reorder';
 import New from '../common/New';
 import { addCategory } from '../../actions/categories';
 
@@ -9,7 +8,6 @@ const Categories = () => (
   <div>
     <New view="categories" onAdd={addCategory} />
     <CategoryList view="categories" />
-    <Reorder />
   </div>
 );
 
