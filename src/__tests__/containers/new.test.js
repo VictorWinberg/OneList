8eb91c561b5f6231709a1571f03851789be8900b
VictorWinberg <victor.m.winberg@gmail.com>
2018-05-08 15:45:58 +0200

    add test for Autosuggest (WIP)

--- src/__tests__/containers/new.test.js
+++ src/__tests__/containers/new.test.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 
 import New from '../../containers/common/New';
 
 const mockStore = configureStore();
 
+const store = mockStore({
+  locale: {
+    languages: [{ code: 'en', active: true }],
+    translations: {},
+  },
+});
+
 describe('New', () => {
   it('should add item', () => {
-    const store = mockStore({
-      locale: {
-        languages: [{ code: 'en', active: true }],
-        translations: {},
-      },
-    });
-
     const context = { store };
     const component = shallow(
       <New view="test" onAdd={item => ({ type: 'ADD', ...item })} />,
@@ -25,5 +26,23 @@ describe('New', () => {
     expect(store.getActions()).toEqual([
       { type: 'ADD', name: 'Milk', category: null },
     ]);
+  });
+
+  it('should add autosuggest item', () => {
+    const component = mount(
+      <Provider store={store}>
+        <New view="test" onAdd={item => ({ type: 'ADD', ...item })} />
+      </Provider>
+    ).find(New);
+
+    // component.find('input').prop('onFocus')();
+    component.find('input').simulate('focus');
+    component.find('input').simulate('change', { target: { value: 'Mi' } });
+    console.log(
+      'focused:',
+      component.find('input').props().id === document.activeElement.id
+    );
+    console.log(component.find('input').html());
+    console.log(store.getActions());
   });
 });
