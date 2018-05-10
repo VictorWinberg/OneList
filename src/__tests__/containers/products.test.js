4a242b53c67c56c0fccd161b2f82c0316be53fe6
VictorWinberg <victor.m.winberg@gmail.com>
2018-05-10 13:50:37 +0200

    add history in test

--- src/__tests__/containers/products.test.js
+++ src/__tests__/containers/products.test.js
@@ -7,10 +7,13 @@ import EditProduct from '../../containers/products/EditProduct';
 
 describe('Products', () => {
   it('should edit a product', () => {
-    const history = { push: () => null };
+    const history = [];
     const wrapper = mount(
       <Provider store={store}>
-        <EditProduct match={{ params: { id: 2 } }} history={history} />
+        <EditProduct
+          match={{ params: { id: 2 } }}
+          history={{ push: url => history.push(url) }}
+        />
       </Provider>
     );
 
@@ -28,7 +31,9 @@ describe('Products', () => {
       .instance().value =
       'Potatoes';
 
+    wrapper.find('.cancelBtn').simulate('click');
     wrapper.find('form').simulate('submit', { preventDefault() {} });
+    expect(history).toEqual(['/', '/']);
     expect(store.getActions()).toEqual([
       { type: 'ADD_CATEGORY', name: 'Potatoes' },
       {
