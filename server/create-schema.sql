1c8d1ee0e5657676cd6be89c58bf77f97424e377
VictorWinberg <victor.m.winberg@gmail.com>
2018-05-12 13:19:14 +0200

    add categories and products sql tables

--- server/create-schema.sql
+++ server/create-schema.sql
@@ -9,3 +9,22 @@ CREATE TABLE users (
 
   PRIMARY KEY (email)
 );
+
+DROP TABLE IF EXISTS categories CASCADE;
+CREATE TABLE categories (
+  id        SERIAL        NOT NULL,
+  name      VARCHAR(255)  NOT NULL,
+  color     VARCHAR(255),
+
+  PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS products;
+CREATE TABLE products (
+  id        SERIAL        NOT NULL,
+  name      VARCHAR(255)  NOT NULL,
+  category  INT,
+
+  FOREIGN KEY(category) REFERENCES categories(id) ON DELETE SET NULL,
+  PRIMARY KEY(id)
+);
