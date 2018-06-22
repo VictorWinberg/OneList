0cd4039a23a5fb7cc997359e047185f68d237bf7
VictorWinberg <victor.m.winberg@gmail.com>
2018-06-22 10:47:25 +0200

    add categories tests

--- src/__tests__/reducers/categories.test.js
+++ src/__tests__/reducers/categories.test.js
@@ -1,6 +1,12 @@
-import { store } from '../store';
+import makeStore, { store } from '../store';
 import categories from '../../reducers/categories';
-import { addCategory, removeCategory } from '../../actions/categories';
+import {
+  addCategory,
+  editCategory,
+  removeCategory,
+  reorderCategory,
+  fetchCategories,
+} from '../../actions/categories';
 
 describe('categories reducer', () => {
   const { dispatch } = store;
@@ -21,6 +27,55 @@ describe('categories reducer', () => {
 
   it('can handle ADD_CATEGORY for None', () => {
     expect(categories(undefined, dispatch(addCategory({})))).toEqual([]);
+  });
+
+  it('can handle EDIT_CATEGORY', () => {
+    expect(
+      categories(
+        [
+          {
+            id: 1,
+            name: 'Vegetables',
+          },
+        ],
+        dispatch(editCategory({ id: 1, name: 'Edit', color: '#fff' }))
+      )
+    ).toEqual([
+      {
+        id: 1,
+        name: 'Edit',
+        color: '#fff',
+      },
+    ]);
+  });
+
+  it('can handle REORDER_CATEGORY', () => {
+    expect(
+      categories(
+        [
+          {
+            id: 1,
+            name: 'Vegetables',
+          },
+          {
+            id: 2,
+            name: 'Meat',
+          },
+        ],
+        dispatch(reorderCategory({ startIndex: 0, endIndex: 1 }))
+      )
+    ).toEqual([
+      {
+        id: 2,
+        name: 'Meat',
+        at: 0,
+      },
+      {
+        id: 1,
+        name: 'Vegetables',
+        at: 1,
+      },
+    ]);
   });
 
   it('can handle REMOVE_CATEGORY', () => {
@@ -44,5 +99,34 @@ describe('categories reducer', () => {
         name: 'Meat',
       },
     ]);
+  });
+
+  it('can handle FETCH_CATEGORIES', done => {
+    const mockStore = makeStore();
+    fetch.mockResponse(
+      JSON.stringify([
+        {
+          id: 1,
+          name: 'Vegetables',
+        },
+      ])
+    );
+
+    mockStore.dispatch(fetchCategories());
+
+    setImmediate(() => {
+      expect(mockStore.getActions()).toEqual([
+        {
+          type: 'FETCH_CATEGORIES',
+          categories: [
+            {
+              id: 1,
+              name: 'Vegetables',
+            },
+          ],
+        },
+      ]);
+      done();
+    });
   });
 });
