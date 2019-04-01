f36862ed26dbf8a566e02050140a5175a50c9876
Victor Winberg <victor.m.winberg@gmail.com>
2019-04-01 22:00:39 +0200

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,10 +3,10 @@ import { Route } from 'react-router-dom';
 
 import Header from './common/Header';
 import ShoppingList from './shoppinglist';
-import EditProduct from './shoppinglist/EditProduct';
 import Categories from './categories';
 import EditCategory from './categories/EditCategory';
 import Products from './products';
+import EditProduct from './products/EditProduct';
 import Settings from './settings';
 import SetLanguage from './settings/SetLanguage';
 import FetchDB from './settings/FetchDB';
@@ -17,10 +17,10 @@ const App = () => (
   <div>
     <Route path="/" component={Header} />
     <Route exact path="/" component={ShoppingList} />
-    <Route path="/shoppinglist/:id" component={EditProduct} />
     <Route exact path="/categories" component={Categories} />
     <Route path="/categories/:id" component={EditCategory} />
     <Route exact path="/products" component={Products} />
+    <Route path="/products/:id" component={EditProduct} />
     <Route path="/settings" component={Settings} />
     <SetLanguage />
     <FetchDB />
