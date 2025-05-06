fb569aeb307cc4fc64bd45c881b36a005f57f7f0
Victor Winberg <victor.m.winberg@gmail.com>
2025-05-06 20:32:25 +0200

    fix: product checked (#243)

--- src/containers/categories/CategoryProductList.js
+++ src/containers/categories/CategoryProductList.js
@@ -1,30 +1,27 @@
 import { connect } from 'react-redux';
 import { getTranslate } from 'react-localize-redux';
-import { flow, forEach, sortBy } from 'lodash/fp';
+import { filter, flow, map, sortBy } from 'lodash/fp';
 
 import { toggleProductInactive } from '../../actions/products';
 import ProductList from '../../components/ProductList';
 
 const active = (state, categoryId) => {
+  const userId = state.user.isCollaboration ? 0 : state.user.id || 0;
   const [category] = state.categories.filter((category) => category.id === categoryId);
-  const items = [];
 
   if (!category) {
-    return [{ value: getTranslate(state.locale)('categories.nonexistent'), items }];
+    return [{ value: getTranslate(state.locale)('categories.nonexistent'), items: [] }];
   }
 
-  flow(
+  const items = flow(
+    map((product) => ({
+      ...product,
+      key: `${product.id}-${product.uid}`,
+      value: product.name,
+      checked: product.uid !== null && product.uid === userId,
+    })),
     sortBy(({ name }) => [name.toLowerCase()]),
-    forEach((product) => {
-      if (product.category === categoryId) {
-        items.push({
-          ...product,
-          key: `${product.id}-${product.uid}`,
-          checked: product.active,
-          value: product.name,
-        });
-      }
-    })
+    filter(({ category }) => category === categoryId)
   )(state.products);
 
   return [{ ...category, value: category.name, items }];
