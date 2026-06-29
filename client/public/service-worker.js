a2211b74e5e334f16e09dd0f7d7d18fec7766a5b
Victor Winberg <victor.m.winberg@gmail.com>
2026-06-29 10:46:25 +0200

    fix: unregister old service-workers

---
+++ client/public/service-worker.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+      .then(() => self.registration.unregister())
+  );
+});
